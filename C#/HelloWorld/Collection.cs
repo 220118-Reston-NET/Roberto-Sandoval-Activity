01f874d5560289e811c10b0ec5e51491dab2c8e0
sandovro <robertosandoval07@gmail.com>
2022-01-19 13:47:48 -0800

    end of day

---
+++ C#/HelloWorld/Collection.cs
@@ -0,0 +1,77 @@
+namespace Collectionfunction{
+    public class Collection
+    {
+        // Array initialization
+        private int[] _nums = new int[5];
+
+        // Generic Collection
+        // List (No set size, specific data type, zero based index)
+        private List<string> strings = new List<string>();
+
+        // Hashset (no specific order, no duplicate elements)
+        private HashSet<int> numbsCollection = new HashSet<int>();
+
+        // Dictionary (no particular order, not zero based index)
+        private Dictionary<string,int> _directory = new Dictionary<string, int>();
+
+
+        // Non-generic Collection
+
+        public void CollectionMain(){
+
+            Console.WriteLine("----");
+
+            _nums[0] = 3;
+            _nums[1] = 10;
+            _nums[2] = 4;
+
+            // Looing through the array
+            Console.WriteLine("--- Foreach Loop ---");
+
+            foreach (int num in _nums){
+                Console.WriteLine(num);
+            }
+
+            // For loops are for more complex manipulation
+            Console.WriteLine("--- For Loop ---");
+
+            for (int i = 0; i < _nums.Length; i++){
+                Console.WriteLine(_nums[i]);
+            }
+
+            Console.WriteLine(">>> Generic Collections <<<");
+
+            Console.WriteLine("--- List ---");
+            strings.Add("First element");
+            strings.Add("Second element");
+            strings.Add("Thrid element");
+            strings.Add("First element");
+
+            foreach (string item in strings){
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(strings[1]);
+
+            Console.WriteLine("--- Hashset ---");
+
+            // Can't index through a hashset
+            numbsCollection.Add(1);
+            numbsCollection.Add(2);
+            numbsCollection.Add(3);
+            numbsCollection.Add(1);
+
+            foreach (int item in numbsCollection){
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("---Dictionary---");
+            // Keys need to be unique but values can repeat
+            _directory.Add("Name1", 10);
+            _directory.Add("Name2", 20);
+            _directory.Add("Name3", 30);
+
+            Console.WriteLine(_directory["Name2"]);
+            Console.WriteLine(_directory["Name3"]);
+        }
+    }
+}
--- C#/HelloWorld/Program.cs
+++ C#/HelloWorld/Program.cs
@@ -28,67 +28,73 @@
 // Console.WriteLine(car2.Color);
 // Console.WireteLine(car2.TotalRange());
 
-Console.Clear();
-Console.WriteLine("Welcome user!");
-Console.WriteLine("What is your name?");
+// Console.Clear();
+// Console.WriteLine("Welcome user!");
+// Console.WriteLine("What is your name?");
 
-string name = Console.ReadLine();
-bool repeat = true;
+// string name = Console.ReadLine();
+// bool repeat = true;
 
-while (repeat){
-    Console.WriteLine($"Hello {name}, what calculation would you like to do today?");
-    Console.WriteLine("Type 1 for addition");
-    Console.WriteLine("Type 2 for subtraction");
-    Console.WriteLine("Type 3 for multiplication");
-    Console.WriteLine("Type 4 for division");
-    Console.WriteLine("Type 5 to exit program");
+// while (repeat){
+//     Console.WriteLine($"Hello {name}, what calculation would you like to do today?");
+//     Console.WriteLine("Type 1 for addition");
+//     Console.WriteLine("Type 2 for subtraction");
+//     Console.WriteLine("Type 3 for multiplication");
+//     Console.WriteLine("Type 4 for division");
+//     Console.WriteLine("Type 5 to exit program");
 
-    int userInput = Convert.ToInt32(Console.ReadLine());
+//     int userInput = Convert.ToInt32(Console.ReadLine());
 
-    if (userInput == 1){
+//     if (userInput == 1){
         
-        Console.WriteLine("What two numbers would you like to add?");
-        int num1 = Convert.ToInt32(Console.ReadLine());
-        int num2 = Convert.ToInt32(Console.ReadLine());
+//         Console.WriteLine("What two numbers would you like to add?");
+//         int num1 = Convert.ToInt32(Console.ReadLine());
+//         int num2 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine($"The result is: {num1+num2}");
-        Console.WriteLine("Press ENTER to continue");
-        Console.ReadLine();
+//         Console.WriteLine($"The result is: {num1+num2}");
+//         Console.WriteLine("Press ENTER to continue");
+//         Console.ReadLine();
 
-    } else if (userInput == 2){
-        Console.WriteLine("What two numbers would you like to subtract?");
-        int num1 = Convert.ToInt32(Console.ReadLine());
-        int num2 = Convert.ToInt32(Console.ReadLine());
+//     } else if (userInput == 2){
+//         Console.WriteLine("What two numbers would you like to subtract?");
+//         int num1 = Convert.ToInt32(Console.ReadLine());
+//         int num2 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine($"The result is: {num1-num2}");
-        Console.WriteLine("Press ENTER to continue");
-        Console.ReadLine();
+//         Console.WriteLine($"The result is: {num1-num2}");
+//         Console.WriteLine("Press ENTER to continue");
+//         Console.ReadLine();
 
-    } else if (userInput == 3){
-        Console.WriteLine("What two numbers would you like to multiply?");
-        int num1 = Convert.ToInt32(Console.ReadLine());
-        int num2 = Convert.ToInt32(Console.ReadLine());
+//     } else if (userInput == 3){
+//         Console.WriteLine("What two numbers would you like to multiply?");
+//         int num1 = Convert.ToInt32(Console.ReadLine());
+//         int num2 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine($"The result is: {num1*num2}");
-        Console.WriteLine("Press ENTER to continue");
-        Console.ReadLine();
+//         Console.WriteLine($"The result is: {num1*num2}");
+//         Console.WriteLine("Press ENTER to continue");
+//         Console.ReadLine();
 
-    } else if (userInput == 4){
-        Console.WriteLine("What two numbers would you like to divide");
-        int num1 = Convert.ToInt32(Console.ReadLine());
-        int num2 = Convert.ToInt32(Console.ReadLine());
+//     } else if (userInput == 4){
+//         Console.WriteLine("What two numbers would you like to divide");
+//         int num1 = Convert.ToInt32(Console.ReadLine());
+//         int num2 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine($"The result is: {num1/num2}");
-        Console.WriteLine("Press ENTER to continue");
-        Console.ReadLine();
+//         Console.WriteLine($"The result is: {num1/num2}");
+//         Console.WriteLine("Press ENTER to continue");
+//         Console.ReadLine();
 
-    } else if (userInput == 5){
-        Console.WriteLine("Thanks for using our calculator. Goodbye!");
-        repeat = false;
+//     } else if (userInput == 5){
+//         Console.WriteLine("Thanks for using our calculator. Goodbye!");
+//         repeat = false;
 
-    } else{
-        Console.WriteLine("You have entered an incorrect option");
-        Console.WriteLine("Press ENTER to continue");
-        Console.ReadLine();
-    }
-}
+//     } else{
+//         Console.WriteLine("You have entered an incorrect option");
+//         Console.WriteLine("Press ENTER to continue");
+//         Console.ReadLine();
+//     }
+// }
+
+using Collectionfunction;
+
+// Initialize Collection class
+Collection collect = new Collection();
+collect.CollectionMain();
