5cd89c4af122a2e2f88cc479a8d89947437afce7
sandovro <robertosandoval07@gmail.com>
2022-01-19 11:15:30 -0800

    update

--- C#/HelloWorld/Car.cs
+++ C#/HelloWorld/Car.cs
@@ -1,9 +1,42 @@
 namespace CarFunction
 {
-
     public class Car
     {
+        // Fields
+        private string _color = "Blue";
+        private string _owner;
+        private int _gallonPerMile;
+
+        // Property
+        public string Color{
+
+            // Getter and setter
+            get{ return "The color is "+_color; }
+            set{ _color = value; }
+        }
+
         
+        public string Owner
+        {
+            get { return _owner; }
+            set { _owner = value; }
+        }
+        
+        public int Fuel { get; set; }
+
+        // Method
+        public void Start(){
+
+            Console.WriteLine("The car is starting up");
+            Console.WriteLine($"Current fuel: {Fuel}");
+        }
+
+        public void Start(int p_fuel){
+
+            Fuel = p_fuel;
+            Console.WriteLine("The car is starting up");
+            Console.WriteLine($"Current fuel: {Fuel}");
+        }
 
     }
 
--- C#/HelloWorld/Program.cs
+++ C#/HelloWorld/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CarFunction;
+
 Console.WriteLine("Hello, World!");
+
+Car car1 = new Car();
+Console.WriteLine(car1.Color);
+car1.Color = "Red";
+Console.WriteLine(car1.Color);
+car1.Fuel = 10;
+Console.WriteLine(car1.Fuel);
+car1.Start();
+car1.Start(100);
